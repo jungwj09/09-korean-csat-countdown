2a153108330eb50f87127f7f5816e7d800d4b827
jungwj09 <woox1jin@gmail.com>
2026-02-28 18:48:22 +0900

    chore: clean up footer copyright

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -4,7 +4,7 @@ export default function Copyright() {
   return (
     <footer className="copyright">
       <p className="copyright-message">이 시간을 버티는 당신을 응원합니다🍀</p>
-      <p className="copyright-legal">ⓒ 2026. Woojin Jung. All rights reserved.</p>
+      <p className="copyright-legal">ⓒ 2026. <a href="https://github.com/jungwj09/09-korean-csat-countdown">09-korean-csat-countdown.</a> All rights reserved.</p>
     </footer>
   );
 }
